8293c0c8980169aba9c5df59732c6bd1588a9ae6
Claire <claire.tortajada@gmail.com>
2022-12-02 15:23:01 -0500

    Page voitureDetail mise a jour

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer className="d-flex justify-content-center">
-            <p className="h6 fw-4">Copyright @ 2022 - Ecom App </p>
+            <p className="h6 fw-4">Copyright @ 2022 </p>
         </footer>
     )
 }
