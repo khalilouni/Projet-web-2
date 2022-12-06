c05a5318b00ba8e56062ad548391fd0653334ea8
Daouda Camara <daoudacamara@MacBook-Air-de-Daouda.local>
2022-12-05 23:18:34 -0500

    Connexion des select Marque et modeles avec la DB

--- app/Models/Constructeur.php
+++ app/Models/Constructeur.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Constructeur extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nom'];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoitureController;
+use App\Http\Controllers\ConstructeurController;
+use App\Http\Controllers\ModeleController;
 use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\ProfilController;
 /*
@@ -18,6 +20,8 @@ use App\Http\Controllers\ProfilController;
 
 Route::group(['prefix' => 'v1'], function() {
     Route::apiResource('voiture', VoitureController::class);
+    Route::apiResource('constructeur', ConstructeurController::class);
+    Route::apiResource('modele', ModeleController::class);
 });
 
 Route::post('v1/register', [AuthController::class,'inscrire'])->name('inscrire');
